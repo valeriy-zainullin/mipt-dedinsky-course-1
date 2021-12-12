08d2ad090a883983a86757891def280a64abc132
Valeriy Zainullin <valeriy.zainullin@yandex.ru>
2021-12-13 02:35:37 +0300

    differentiator: add main file.

--- differentiator/src/main.cpp
+++ differentiator/src/main.cpp
@@ -5,5 +5,23 @@
 #include <stdlib.h>
 
 int main() {
+	Reader reader = {};
+	if (!reader_init(&reader, stdin, true)) {
+		fprintf(stderr, "Не удалось инициализировать читатель.\n");
+		return 1;
+	}
+	
+	printf("Введите выражение: ");
+	fflush(stdout);
+	
+	Tree tree = {};
+	
+	if (!parsing_read_expression(reader, tree)) {
+		fprintf(stderr, "Не удалось разобрать выражение.");
+		return 2;
+	}
+	
+	reader_deinit(&reader);
+	
 	return 0;
 }
