8c037654d8d337ceb5a2e44bf7b6db31b40d93d8
Valeriy Zainullin <valeriy.zainullin@yandex.ru>
2021-12-13 13:03:28 +0300

    differentiator: add latex output writing.

--- differentiator/src/main.cpp
+++ differentiator/src/main.cpp
@@ -1,14 +1,24 @@
-#include "differentiation.h"
-#include "parsing.h"
+#include "differentiation/differentiation.h"
+#include "input/parsing.h"
 
 #include <stddef.h>
 #include <stdlib.h>
 
-int main() {
+int main(int argc, char** argv) {
+	if (argc != 2) {
+		return 1;
+	}
+	FILE* output_stream = fopen(argv[1]);
+	if (output_stream == NULL) {
+		fprintf(stderr, "Не удалось открыть выходной файл.\n");
+		return 2;
+	}
+
 	Reader reader = {};
 	if (!reader_init(&reader, stdin, true)) {
 		fprintf(stderr, "Не удалось инициализировать читатель.\n");
-		return 1;
+		fclose(output_stream);
+		return 3;
 	}
 	
 	printf("Введите выражение: ");
@@ -17,11 +27,15 @@ int main() {
 	Tree tree = {};
 	
 	if (!parsing_read_expression(reader, tree)) {
-		fprintf(stderr, "Не удалось разобрать выражение.");
-		return 2;
+		fprintf(stderr, "Не удалось разобрать выражение.\n");
+		fclose(output_stream);
+		return 4;
 	}
 	
+	latex_write(output_stream, tree);
+	
 	reader_deinit(&reader);
+	fclose(output_stream);
 	
 	return 0;
 }
