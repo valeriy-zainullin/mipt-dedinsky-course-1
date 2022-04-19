f883873819ea8c3c934c7f20e69b5f6f0bc2dd45
Valeriy Zainullin <valeriy.zainullin@yandex.ru>
2022-04-19 11:29:36 +0300

    2-06: publish some driver tools.

---
+++ 2-06-hack_program_of_a_groupmate/solutions/3-patching-driver(driver-crack)/enable_debugging.cmd
@@ -0,0 +1,5 @@
+bcdedit /debug on
+bcdedit /dbgsettings serial DEBUGPORT:1 BAUDRATE:115200
+bcdedit /set testsigning on
+bcdedit /dbgsettings
+pause
---
+++ 2-06-hack_program_of_a_groupmate/solutions/3-patching-driver(driver-crack)/install_driver.cmd
@@ -0,0 +1,2 @@
+pnputil -i -a myminifilter\x64\Debug\myminifilter.inf
+pause
