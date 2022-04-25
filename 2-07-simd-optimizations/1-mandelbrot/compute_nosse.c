31e4f3276e313a67287cd0b4ebadc7c2c8f17de5
Valeriy Zainullin <valeriy.zainullin@yandex.ru>
2022-04-25 19:53:53 +0300

    2-07-1: implement no-sse version of mandelbrot.

---
+++ 2-07-simd-optimizations/1-mandelbrot/compute_nosse.c
@@ -0,0 +1,57 @@
+#include "compute.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void compute_nosse(struct rgba * buffer, struct screen_state* screen_state) {
+	for (size_t row = 0; row < SCREEN_ROWS; ++row) {
+		for (size_t col = 0; col < SCREEN_COLS; ++col) {
+			size_t pos = row * SCREEN_COLS + col;
+
+			float pixel_x = screen_state->center.x + screen_state->scale * ((int) col - (int) SCREEN_COLS / 2);
+			float pixel_y = screen_state->center.y + screen_state->scale * ((int) SCREEN_ROWS / 2 - (int) row);
+			
+			// Вычисляем элементы последовательности z_{n + 1} = z_n^2 + c, где c --- началаная
+			// точка на комплексной плоскости, c = pixel_x + i * pixel_y.
+
+			float starting_x = pixel_x;
+			float starting_y = pixel_y;
+			float x = starting_x;
+			float y = starting_y;
+			size_t iteration = 0;
+			for (; iteration < MAX_NUM_ITERATIONS; ++iteration) {
+				float x_sq = x * x;
+				float xy = x * y;
+				float y_sq = y * y;
+				
+				// (re + i * im) ^ 2 = re^2 + 2 * re * i * im - im^2
+				// x = Re((re + i * im)^2) + starting_x
+				// y = Im((re + i * im)^2) + starting_y
+				x = x_sq - y_sq + starting_x;
+				y = 2 * xy + starting_y;
+
+				float r_sq = x * x + y * y;
+				
+				// Если точка вышла за максимальный радиус, считаем, что она уже улетит на
+				// бесконечность. И можем посчитать её цвет.
+				if (r_sq >= MAX_ALLOWED_MODULO_SQ) {
+					break;
+				}
+			}
+			
+			// Не улетела на бесконечность.
+			// Где-то в окружности радиуса 1 с центром в 0 бегало.
+			if (iteration == MAX_NUM_ITERATIONS) {
+				buffer[pos].red = 0;
+				buffer[pos].green = 0;
+				buffer[pos].blue = 0;
+				buffer[pos].alpha = 255;
+			} else {
+				buffer[pos].red = iteration % 64;
+				buffer[pos].green = 64 + iteration % 64;
+				buffer[pos].blue = 128 + iteration % 64;
+				buffer[pos].alpha = 255;
+			}
+		}
+	}
+}
---
+++ 2-07-simd-optimizations/1-mandelbrot/view_main.c
@@ -0,0 +1,131 @@
+#include "compute.h"
+
+#include <SDL2/SDL.h>
+
+#include <windows.h>
+
+#include <stdbool.h>
+#include <stdlib.h>
+
+// Workaround strange SDL2 behaviour when it redefines main function, but then with -lSDL2Main -lSDL2 it doesn't compile.
+// Based on https://stackoverflow.com/a/29848468
+#if defined(main)
+#undef main
+#endif
+
+int main() {
+	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+		fprintf(stderr, "SDL_Init failed: %s.\n", SDL_GetError());
+		MessageBoxW(NULL, L"Ошибка при инициализации.", L"Ошибка SDL2", 0);
+		return 1;
+	}
+	
+	SDL_Window* window = SDL_CreateWindow("Множество Мандельброта", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_COLS, SCREEN_ROWS, SDL_WINDOW_SHOWN);
+	if (window == NULL) {
+		fprintf(stderr, "SDL_CreateWindow failed: %s.\n", SDL_GetError());
+		MessageBoxW(NULL, L"Ошибка при создании окна.", L"Ошибка SDL2", 0);
+		SDL_Quit();
+		return 2;
+	}
+	
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (renderer == NULL) {
+		fprintf(stderr, "SDL_CreateRenderer failed: %s.\n", SDL_GetError());
+		MessageBoxW(NULL, L"Ошибка при создании отрисовщика.", L"Ошибка SDL2", 0);
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return 3;
+	}
+	
+	// Looks like SDL orders these fields big-endian, so for the structure those will be RGBA.
+	SDL_Texture* texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ABGR8888, SDL_TEXTUREACCESS_STREAMING, SCREEN_COLS, SCREEN_ROWS);
+	if (texture == NULL) {
+		fprintf(stderr, "Failed to create texture. SDL_CreateTexture failed: %s.\n", SDL_GetError());
+		MessageBoxW(NULL, L"Ошибка при создании текстуры", L"Ошибка SDL2", 0);
+		SDL_DestroyRenderer(renderer);
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return 5;
+	}
+
+	// TODO: проверить с санитайзерами.
+	bool exiting = false;
+	bool using_sse = false;
+	
+	struct screen_state screen_state = INITIAL_SCREEN_STATE;
+	
+	while (!exiting) {
+		SDL_Event event = {0};
+		
+		while (SDL_PollEvent(&event)) {
+			switch (event.type) {
+				case SDL_KEYDOWN: {
+					switch (event.key.keysym.scancode) {
+						// TODO: consider precision losses here by limiting scaling and moving.
+						case SDL_SCANCODE_RETURN: {
+							screen_state.scale /= SCALE_TIMES_STEP;
+							break;
+						}
+						
+						case SDL_SCANCODE_SPACE: {
+							screen_state.scale *= SCALE_TIMES_STEP;
+							break;
+						}
+						
+						case SDL_SCANCODE_LEFT: {
+							screen_state.center.x -= PIXEL_STEP * screen_state.scale;
+							break;
+						}
+						
+						case SDL_SCANCODE_RIGHT: {
+							screen_state.center.x += PIXEL_STEP * screen_state.scale;
+							break;
+						}
+						
+						case SDL_SCANCODE_UP: {
+							screen_state.center.y += PIXEL_STEP * screen_state.scale;
+							break;
+						}
+
+						case SDL_SCANCODE_DOWN: {
+							screen_state.center.y -= PIXEL_STEP * screen_state.scale;
+							break;
+						}
+						
+						default: break;
+					}
+					break;
+				}
+				
+				case SDL_QUIT: {
+					exiting = true;
+					break;
+				}
+			}
+		}
+		
+		int row_size = 0;                                              // in bytes
+		struct rgba* pixels = NULL;
+		SDL_LockTexture(texture, NULL, (void**) &pixels, &row_size);
+		
+		if (using_sse) {
+			compute_sse(pixels, &screen_state);
+		} else {
+			compute_nosse(pixels, &screen_state);
+		}
+						
+		SDL_UnlockTexture(texture);
+		
+		SDL_RenderClear(renderer);
+		SDL_RenderCopy(renderer, texture, NULL, NULL);
+		SDL_RenderPresent(renderer);
+	}
+	
+	SDL_DestroyRenderer(renderer);
+	
+	SDL_DestroyWindow(window);
+	
+	SDL_Quit();
+
+	return 0;
+}
